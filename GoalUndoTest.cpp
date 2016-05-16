896d8a435c6f92c81b6f2a23eac6a779102f1738
stulsani <sumeettulsani1@gmail.com>
2016-05-15 23:16:13 -0700

    Add Operation function Tested

--- GoalUndoTest.cpp
+++ GoalUndoTest.cpp
@@ -4,7 +4,7 @@
 
 #include <gtest/gtest.h>
 #include "GoalUndo.h"
- 
+
 class GoalUndoTest : public ::testing::Test
 {
 	protected:
@@ -17,4 +17,34 @@ class GoalUndoTest : public ::testing::Test
 TEST(GoalUndoTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
+}
+
+TEST(GoslUndoTest, AddFirstGoal)
+{
+    GoalUndo Goaladd;
+    Goaladd.addOperation("Fitness", "Running");
+    ASSERT_EQ("Fitness", Goaladd.getGoal() );
+}
+
+TEST(GoslUndoTest, AddEmptyGoal)
+{
+    GoalUndo GoalEmpty;
+    GoalEmpty.addOperation("", "Walking");
+    ASSERT_EQ("", GoalEmpty.getGoal() );
+}
+
+TEST(GoslUndoTest, AddEmptyOperation)
+{
+    GoalUndo OperationEmpty;
+    OperationEmpty.addOperation("Fitness", "");
+    ASSERT_EQ("", OperationEmpty.getGoal() );
+}
+
+TEST(GoslUndoTest, AddGoals)
+{
+    GoalUndo addGoals;
+    addGoals.addOperation("Fitness", "Running");
+    addGoals.addOperation("Good Grades", "Study");
+    addGoals.addOperation("","Running");
+    ASSERT_EQ("Good Grades", addGoals.getGoal() );
 }
